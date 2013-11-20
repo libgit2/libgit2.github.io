e4e0725616a3f63e8b14ef0aac0559af9c5222da
Ben Straub <bs@github.com>
2013-11-20 10:57:42 -0800

    Generate TOC

--- js/toc.js
+++ js/toc.js
@@ -1,7 +1,29 @@
-$(window).scroll(function(){
-  $("#toc")
-    .stop()
-    .animate({"marginTop": ($(window).scrollTop()) + "px",
-              "marginLeft":($(window).scrollLeft()) + "px"},
-             "fast");
+$(function () {
+  var toc = $('#toc');
+  if (toc.length == 0) {
+    return;
+  }
+
+  // Keep the TOC in view
+  $(window).scroll(function(){
+    $("#toc")
+      .stop()
+      .animate({"marginTop": ($(window).scrollTop()) + "px",
+                "marginLeft":($(window).scrollLeft()) + "px"},
+               "fast");
+  });
+
+  // Generate the TOC contents
+  var current_level = 0;
+  $('#primary :header[id]').each(function(i, el) {
+    var del = $(el);
+    console.log(el);
+
+    var div = $('<div></div>')
+      .addClass('toc-' + el.nodeName.toLowerCase());
+    div.append($('<a></a>')
+      .attr('href', '#' + del.attr('id'))
+      .text(del.text()));
+    toc.append(div);
+  });
 });
